3f2d95c75711d07c91b32da486a9f15fc577f58d
mykhailokrachun <mykhailokrachun@gmail.com>
2023-04-05 16:13:05 +0300

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>About us</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet aliquam
+        quidem quam eligendi. Dolorem, cumque veniam! Ipsa quo quisquam, laborum
+        praesentium nulla dolor architecto voluptatibus nam itaque facere,
+        repellat dolorem.
+      </p>
+    </section>
+  );
+};
+
+export default About;
---
+++ src/pages/SharedLayout.js
@@ -0,0 +1,12 @@
+import Navbar from '../components/Navbar';
+import { Outlet } from 'react-router-dom';
+
+const SharedLayout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  );
+};
+export default SharedLayout;
